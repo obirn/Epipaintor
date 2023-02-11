f4d7e3282441aa53d2b3b2f93614cfd83a471b4e
Christian AZIAKA <christian.aziaka@epita.fr>
2023-02-11 19:38:52 +0100

    save button color paletted  etc added

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -20,6 +20,7 @@ GtkWidget *fixed2;
 GtkWidget *draw_area;
 GtkWidget *brush;
 
+GtkWidget *save_file_button;
 char* image_path;
 
 int selected_tool = NONE;
@@ -67,17 +68,18 @@ int init_interface(int argc, char**argv)
 	label = GTK_WIDGET(gtk_builder_get_object(builder,"label"));
 	draw_area = GTK_WIDGET(gtk_builder_get_object(builder,"draw_area"));
 	brush = GTK_WIDGET(gtk_builder_get_object(builder,"brush_button"));
+	save_file_button = GTK_WIDGET(gtk_builder_get_object(builder,"save_window"));
 
 	// Create events
-    gtk_widget_add_events(draw_area, GDK_POINTER_MOTION_MASK);
-    gtk_widget_add_events(draw_area, GDK_BUTTON_PRESS_MASK);
-    gtk_widget_add_events(draw_area, GDK_BUTTON_RELEASE_MASK);
+	gtk_widget_add_events(draw_area, GDK_POINTER_MOTION_MASK);
+	gtk_widget_add_events(draw_area, GDK_BUTTON_PRESS_MASK);
+	gtk_widget_add_events(draw_area, GDK_BUTTON_RELEASE_MASK);
 
 	// Connecting signals
 	gtk_builder_connect_signals(builder,NULL);
 	g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
 	g_signal_connect(openfilebutton,"file-set",G_CALLBACK(on_open_file_file_activated),NULL);
-    g_signal_connect (G_OBJECT (draw_area), "draw", G_CALLBACK (draw_callback), NULL);
+	g_signal_connect (G_OBJECT (draw_area), "draw", G_CALLBACK (draw_callback), NULL);
 	g_signal_connect (draw_area, "motion-notify-event", G_CALLBACK(mouse_on_move), NULL);
     g_signal_connect (draw_area, "button-press-event", G_CALLBACK(mouse_on_press), NULL);
     g_signal_connect (draw_area, "button-release-event", G_CALLBACK(mouse_on_release), NULL);
@@ -95,56 +97,57 @@ int init_interface(int argc, char**argv)
 
 gboolean draw_callback(GtkWidget* widget, cairo_t *cr, gpointer data)
 {
-    //Unused parameters :
-    widget = widget;
-    data = data;
+	//Unused parameters :
+	widget = widget;
+	data = data;
 
 	if (!img_buff) return FALSE;
 
-    //Actual function :
-    SDL_SaveBMP(img_buff, "../cache/img_buff.bmp");
-    GdkPixbuf *pixbuf;
+	//Actual function :
+	SDL_SaveBMP(img_buff, "../cache/img_buff.bmp");
+	GdkPixbuf *pixbuf;
 
-    pixbuf = gdk_pixbuf_new_from_file("../cache/img_buff.bmp", NULL);
+	pixbuf = gdk_pixbuf_new_from_file("../cache/img_buff.bmp", NULL);
 
-    gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
+	gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
 
-    cairo_paint(cr);
+	cairo_paint(cr);
 
-    if (pixbuf)
-        g_object_unref(pixbuf);
+	if (pixbuf)
+		g_object_unref(pixbuf);
 
-    return FALSE;
+	return FALSE;
 }
 
 gboolean on_open_file_file_activated(GtkFileChooserButton * b)
 {
-	
+
 	image_path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(b));
 
 	img_buff = load_image(image_path);
 
-    gtk_widget_queue_draw_area(draw_area,0,0,img_buff->w,img_buff->h);
-    
-    int w = 1310;
-    int h = 903;
-    if(img_buff->w > 1080)
-    {
-        w = img_buff->w+230;
-        gtk_widget_set_margin_start(GTK_WIDGET(draw_area), 0);
-    }
-    else
-        gtk_widget_set_margin_start(GTK_WIDGET(draw_area), (w-230-img_buff->w)/2);
+	gtk_widget_queue_draw_area(draw_area,0,0,img_buff->w,img_buff->h);
 
-    if(img_buff->h > 850)
-    {
-        h = img_buff->h+53;
-        gtk_widget_set_margin_top(GTK_WIDGET(draw_area), 0);
-    }
-    else
-        gtk_widget_set_margin_top(GTK_WIDGET(draw_area), (h-53-img_buff->h)/2);
+	
+	int w = 1310;
+	int h = 903;
+	if(img_buff->w > 1080)
+	{
+	w = img_buff->w+230;
+	gtk_widget_set_margin_start(GTK_WIDGET(draw_area), 0);
+	}
+	else
+	gtk_widget_set_margin_start(GTK_WIDGET(draw_area), (w-230-img_buff->w)/2);
 
-    gtk_window_resize(GTK_WINDOW(window), w, h);     
+	if(img_buff->h > 850)
+	{
+	h = img_buff->h+53;
+	gtk_widget_set_margin_top(GTK_WIDGET(draw_area), 0);
+	}
+	else
+	gtk_widget_set_margin_top(GTK_WIDGET(draw_area), (h-53-img_buff->h)/2);
+
+	gtk_window_resize(GTK_WINDOW(window), w, h);     
 
     g_free(image_path);
 
@@ -153,36 +156,36 @@ gboolean on_open_file_file_activated(GtkFileChooserButton * b)
 
 gboolean mouse_on_press(GtkWidget* self, GdkEvent* event, gpointer user_data)
 {
-    // printf("Mouse on press\n");
-    widget = self;
-    event = event;
+	// printf("Mouse on press\n");
+	widget = self;
+	event = event;
 
-    if(user_data == NULL)
-    {
-        is_pressed = TRUE;
-        start_x = pos_x;
-        start_y = pos_y;
-        //printf("Start coordinates: (%u,%u)\n", start_x, start_y);
-    }
+	if(user_data == NULL)
+	{
+		is_pressed = TRUE;
+		start_x = pos_x;
+		start_y = pos_y;
+		//printf("Start coordinates: (%u,%u)\n", start_x, start_y);
+	}
 
-    return FALSE;
+	return FALSE;
 }
 
 gboolean mouse_on_release(GtkWidget* self, GdkEvent* event, gpointer user_data)
 {
-    widget = self;
-    event = event;
+	widget = self;
+	event = event;
 
-    // printf("Mouse on release\n");
-    if(user_data == NULL)
-    {
-        is_pressed = FALSE;
-        start_x = pos_x;
-        start_y = pos_y;
-        //printf("Start coordinates: (%u,%u)\n", start_x, start_y);
-    }
+	// printf("Mouse on release\n");
+	if(user_data == NULL)
+	{
+		is_pressed = FALSE;
+		start_x = pos_x;
+		start_y = pos_y;
+		//printf("Start coordinates: (%u,%u)\n", start_x, start_y);
+	}
 
-    return FALSE;
+	return FALSE;
 }
 
 
@@ -195,13 +198,13 @@ gboolean mouse_on_move(GtkWidget *widget,GdkEvent *event, gpointer user_data)
     widget = widget;
 	
 
-    if (event->type==GDK_MOTION_NOTIFY && user_data == NULL) 
-    {
-        GdkEventMotion* e =(GdkEventMotion*)event;
-        old_x = pos_x;
-        old_y = pos_y;
-        pos_x = (guint) e->x;
-        pos_y = (guint) e->y;
+	if (event->type==GDK_MOTION_NOTIFY && user_data == NULL) 
+	{
+		GdkEventMotion* e =(GdkEventMotion*)event;
+		old_x = pos_x;
+		old_y = pos_y;
+		pos_x = (guint) e->x;
+		pos_y = (guint) e->y;
 
         //printf("Old coordinates: (%u,%u)\n", old_x, old_y);
         //printf("coordinates: (%u,%u)\n", pos_x, pos_y);
@@ -228,3 +231,10 @@ gboolean on_brush(GtkButton *self, gpointer user_data) {
 
 	return FALSE;
 }
+void on_save_button_clicked(GtkButton *b)
+{
+	SDL_Surface * img_buff = SDL_LoadBMP("../cache/img_buff.bmp");
+	SDL_SaveBMP(img_buff,"../save/saved_img.bmp");
+
+}
+
